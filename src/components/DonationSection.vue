5fbb3eb721086bd0d6e3b80eaedee83d7d57de28
Gianluca Lorenzon <gianluca.lorenzon@cloudnine.de>
2024-11-26 14:21:08 +0100

    feat: refactor BarChart and DonationSection components for improved readability and layout

--- src/components/DonationSection.vue
+++ src/components/DonationSection.vue
@@ -4,17 +4,17 @@
       <span class="font-bold text-4xl">3.</span>
       Ihr Beitrag für den Heidefonds+
     </h3>
-    <p class="text-lg text-gray mt-10 drop-shadow-md">Möchtest du deine
+    <p class="text-lg text-gray mt-10 drop-shadow-md">
       Wir freuen uns über Ihre Spende an den Heidefonds+ Lüneburger Heide. Wir
       empfehlen eine Spende in Höhe der berechneten Umweltfolgekosten
       (voreingestellt). Gerne können Sie aber auch einen individuellen Betrag
       spenden. Vielen Dank für Ihr Engagement und besuchen Sie uns gerne wieder!
     </p>
 
-    <div class="grid grid-cols-3 gap-6 mt-10">
+    <div class="grid xl:grid-cols-3 gap-6 mt-10">
       <PaypalButton clientId="YOUR_CLIENT_ID" :amount="5" />
-      <PaypalButton clientId="YOUR_CLIENT_ID" :amount="compensation" />
-      <PaypalButton clientId="YOUR_CLIENT_ID" :amount="compensation + 10" />
+      <!-- <PaypalButton clientId="YOUR_CLIENT_ID" :amount="compensation" />
+      <PaypalButton clientId="YOUR_CLIENT_ID" :amount="compensation + 10" /> -->
     </div>
   </div>
 </template>
